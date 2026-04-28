cb3b68a0ea2a1eb595895b463f96f7324cf2e4da
lloyet <contact@lloyet.dev>
2026-04-28 13:39:32 +0200

    fix(cI): switch image minecraft server

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 import express from "express";
 import { Rcon } from "rcon-client";
 
+const HOST = process.env.HOST ?? "0.0.0.0";
+const PORT = Number.parseInt(process.env.PORT ?? "3000");
+const RCON_HOST = process.env.RCON_HOST ?? "0.0.0.0";
+const RCON_PORT = Number.parseInt(process.env.RCON_PORT ?? "25575");
+const RCON_PASSWORD = process.env.RCON_PASSWORD ?? "1234";
+
 /**
  * @type {Rcon}
  */
 export let rcon;
 
 Rcon.connect({
-	host: "localhost",
-	port: 25575,
-	password: "1234",
+	host: RCON_HOST,
+	port: RCON_PORT,
+	password: RCON_PASSWORD,
 }).then((connection) => {
-	console.log("[RCON] Connected at 0.0.0.0:25575.");
+	console.log(`[RCON] Connected at ${RCON_HOST}:${RCON_PORT}.`);
 	rcon = connection;
 
 	rcon.on("end", () => {
-		console.log("[RCON] Disconnected from 0.0.0.0:25575.");
+		console.log(`[RCON] Disconnected from ${RCON_HOST}:${RCON_PORT}.`);
 	});
 });
 
@@ -32,6 +38,6 @@ app.post("/mobs", async (req, res) => {
 	res.status(201).send(rconResponse);
 });
 
-export const server = app.listen(3000, () => {
-	console.log("Server is running on http://localhost:3000");
+export const server = app.listen(PORT, HOST, () => {
+	console.log(`Server is running on http://${HOST}:${PORT}`);
 });
